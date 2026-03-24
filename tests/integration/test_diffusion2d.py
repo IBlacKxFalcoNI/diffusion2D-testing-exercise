5b3fde55d4194b1643ddd7a6ec80d0c1f30ddc30
IBlacKxFalcoNI <alexander.kaiser136@yahoo.de>
2026-03-24 16:46:21 +0100

    add integration test and report as pdf

--- tests/integration/test_diffusion2d.py
+++ tests/integration/test_diffusion2d.py
@@ -2,6 +2,7 @@
 Tests for functionality checks in class SolveDiffusion2D
 """
 
+import pytest
 from diffusion2d import SolveDiffusion2D
 
 
@@ -10,6 +11,10 @@ def test_initialize_physical_parameters():
     Checks function SolveDiffusion2D.initialize_domain
     """
     solver = SolveDiffusion2D()
+    solver.initialize_domain(w=20., h=10., dx=0.05, dy=0.05)
+    solver.initialize_physical_parameters(d=5., T_cold=650., T_hot=750.)
+    expected_dt = pytest.approx(0.000125, abs=1e-9)
+    assert solver.dt == expected_dt
 
 
 def test_set_initial_condition():
@@ -17,3 +22,9 @@ def test_set_initial_condition():
     Checks function SolveDiffusion2D.get_initial_function
     """
     solver = SolveDiffusion2D()
+    solver.initialize_domain(w=20., h=10., dx=0.05, dy=0.05)
+    solver.initialize_physical_parameters(d=5., T_cold=650., T_hot=750.)
+    u0 = solver.set_initial_condition()
+    expected_u0_center = 750.
+    actual_u0_center = u0[solver.nx // 2, solver.ny // 2]
+    assert actual_u0_center == expected_u0_center
